123206d5517411dfcd27f989f6f015123b07284e
rstayers <rstayers@nd.edu>
2025-11-30 08:33:58 -0600

    LN

---
+++ Imagenet-LN/nuisances/__init__.py
@@ -0,0 +1,8 @@
+from .base import LocalNuisance
+from .sticker import StickerNuisance
+
+__all__ = ["LocalNuisance", "StickerNuisance"]
+from .base import LocalNuisance
+from .sticker import StickerNuisance
+
+__all__ = ["LocalNuisance", "StickerNuisance"]
